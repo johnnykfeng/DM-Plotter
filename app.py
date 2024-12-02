16fe47c87358c8da46592f064faeb4856416c24e
John Feng <johnfengphd@gmail.com>
2024-12-02 15:37:55 -0800

    implemented bin selector

--- app.py
+++ app.py
@@ -33,8 +33,7 @@ if uploaded_files:
     st.write(uploaded_files[0].name)
     extracted_files = [file for file in uploaded_files]
     file_type = type(extracted_files[0])
-    # st.write(file_type)
-    # print(file_type)
+
     with st.expander("Metadata", expanded=False):
         msgs, params = get_data_info(extracted_files, check_mat=False)
         for msg in msgs:
@@ -47,19 +46,30 @@ if uploaded_files:
         for file in extracted_files:
             st.write(file.name)
 
-    for i, bin_id in enumerate(range(7)):
+    def bin_id_to_label(bin_id):
+        return BIN_LABELS[bin_id]
+
+    bin_selection = st.multiselect(
+        "Select bins",
+        [0, 1, 2, 3, 4, 5, 6],
+        default=[0, 1, 2, 3, 4, 5, 6],
+        format_func=bin_id_to_label,
+    )
+
+    for i, bin_id in enumerate(bin_selection):
         _, _, full_count_map = process_mat_files_list(
             bin_id, extracted_files, file_check=False
         )
 
         color_min, color_max = np.percentile(full_count_map, [1, 99.5])
 
-        with st.expander(f"{BIN_LABELS[bin_id]}", expanded=True):
-            color_range = st.slider(
-                "Color range", 0.0, color_max * 2, (color_min, color_max)
-            )
+        # with st.expander(f"{BIN_LABELS[bin_id]}", expanded=True):
+        st.header(f"{BIN_LABELS[bin_id]}")
+        color_range = st.slider(
+            "Color range", 0.0, color_max * 2, (color_min, color_max)
+        )
 
-            heatmap_fig = create_heatmap(full_count_map, color_range)
-            heatmap_fig.update_layout(title=f"{BIN_LABELS[bin_id]}")
-            heatmap_fig.update_layout(autosize=False, width=400, height=600)
-            st.plotly_chart(heatmap_fig, key=f"heatmap_{i}")
+        heatmap_fig = create_heatmap(full_count_map, color_range)
+        heatmap_fig.update_layout(title=f"{BIN_LABELS[bin_id]}")
+        heatmap_fig.update_layout(autosize=False, width=400, height=600)
+        st.plotly_chart(heatmap_fig, key=f"heatmap_{bin_id}")
